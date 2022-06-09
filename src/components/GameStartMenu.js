c6ac12fc1bba2f93dda80a31bd5604b918946e46
nad1a0S <32828052+nad1a0S@users.noreply.github.com>
2022-06-09 16:43:31 -0400

    pokeball menu hover effect

--- src/components/GameStartMenu.js
+++ src/components/GameStartMenu.js
@@ -17,16 +17,19 @@ const GameStartMenu = () => {
                 <Carousel dragging={true} >
                     <Link to="/Game">
                         <Button className="menuButton" onClick={() => dispatch({type: 'SELECT_GAME_1'})}>
+                            <img src="./images/greatball.png" alt="http://placecorgi.com/250" className="menuBallImage" />
                             <img src="./images/pokemon-level1.jpeg" alt="http://placecorgi.com/250" className="menuImage" />
                         </Button>
                     </Link >
                     <Link to="/Game">
                         <Button className="menuButton" onClick={() => dispatch({type: 'SELECT_GAME_2'})}>
+                            <img src="./images/ultraball.png" alt="http://placecorgi.com/250" className="menuBallImage" />
                             <img src="./images/pokemon-level2.webp" alt="http://placecorgi.com/250" className="menuImage"/>
                         </Button>
                     </Link>
                     <Link to="/Game">
                         <Button className="menuButton" onClick={() => dispatch({type: 'SELECT_GAME_3'})}>
+                        <img src="./images/masterball.png" alt="http://placecorgi.com/250" className="menuBallImage" />
                             <img src="./images/pokemon-level3.jpeg" alt="http://placecorgi.com/250" className="menuImage"/>
                         </Button>
                     </Link>
